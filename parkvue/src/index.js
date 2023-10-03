031e5437099cfbc6d6193f71f3769f750fc4480c
sskyhigh <98570636+sskyhigh@users.noreply.github.com>
2023-10-02 23:55:00 -0400

    made small changes, testing

--- parkvue/src/index.js
+++ parkvue/src/index.js
@@ -5,6 +5,7 @@ import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <div>placeholder text</div>
+    <div>Placeholder text</div>
+    <div>Made Changes</div>
   </React.StrictMode>
 );
