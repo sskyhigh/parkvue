3e8c44ed955d5ed1d52a4daf6f50344e239b4b4f
Kami840 <56164472+Kami840@users.noreply.github.com>
2023-10-21 12:31:45 -0400

    NavBar Updated

---
+++ parkvue/src/Components/SignIn.js
@@ -0,0 +1,14 @@
+import '../../src/App.css';
+
+function SignIn() {
+  return (
+    <div>
+      <button>Sign In</button>
+      {/* <a href="#" class="sign_in">Sign In</a> */}
+    </div>
+  );
+}
+
+
+
+export default SignIn;
---
+++ parkvue/src/Components/SignUp.js
@@ -0,0 +1,14 @@
+import '../../src/App.css';
+
+function SignUp() {
+  return (
+    <div>
+      <button>Sign Up</button>
+      {/* <a href="#" class="sign_up">Sign Up</a> */}
+    </div>
+  );
+}
+
+
+
+export default SignUp;
