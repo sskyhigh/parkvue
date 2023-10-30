93bb58da3987b3594f2c9ddbd51924508b28d794
sskyhigh <98570636+sskyhigh@users.noreply.github.com>
2023-10-30 02:14:39 -0400

    updated input fields

--- parkvue/src/Pages/Login.jsx
+++ parkvue/src/Pages/Login.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function Login() {
-  return (
-    <div>Login</div>
-  )
+    const [data, setData] = useState({
+        email: "",
+        password: ""
+    })
+
+    const login = async (event) => {
+        event.preventDefault();
+    }
+
+    return (
+        <div>
+            <form onSubmit={login}>
+                <label htmlFor="">Email: </label>
+                <input
+                    type="text"
+                    placeholder={"something@something.com"}
+                    value={data.email}
+                    onChange={(event) => setData({...data, email: event.target.value})}
+                />
+                <label htmlFor="">Email: </label>
+                <input
+                    type="text"
+                    placeholder={"****"}
+                    value={data.password}
+                    onChange={(event) => setData({...data, password: event.target.value})}
+                />
+            </form>
+        </div>
+    )
 }
