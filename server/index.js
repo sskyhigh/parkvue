4dc82f7ae86cdebe158b6e28c209bd33398366da
sskyhigh <98570636+sskyhigh@users.noreply.github.com>
2023-11-01 00:08:42 -0400

    made changes

--- server/index.js
+++ server/index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 const cors = require('cors')
+const {mongoose} = require('mongoose')
+
+//connecting to database3
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => console.log("Database has been connected"))
+    .catch((error) => console.log("Database is not connected", error))
 
 const app = express();
 app.use('/', require('./routes/authRoutes'))
