3c763607de1fcaa5e2edb06c451656232f523514
sskyhigh <98570636+sskyhigh@users.noreply.github.com>
2023-10-31 21:48:18 -0400

    made small changes

--- parkvue/src/Components/NavBar.js
+++ parkvue/src/Components/NavBar.js
@@ -10,15 +10,15 @@ function NavBar() {
                 {/* <img src={logo} alt={'test'}/> */}
             </a>
 
-            <div class="bx_bx-menu" id="menu-icon"></div>
-            <ul class="navbar">
+            <div className="bx_bx-menu" id="menu-icon"></div>
+            <ul className="navbar">
                 <li><a href="#home">Home</a></li>
                 <li><a href="#map">Map</a></li>
                 <li><a href="#rewards">Rewards</a></li>
                 <li><a href="#about">About</a></li>
             </ul>
 
-            <div class="header-btn">
+            <div className="header-btn">
                 <SignUp/>
                 <SignIn/>
             </div>
