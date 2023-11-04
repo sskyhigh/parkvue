1a5aeafa75edcd2c083ecdc95d0d07ff357e65d0
sskyhigh <98570636+sskyhigh@users.noreply.github.com>
2023-11-04 03:11:27 -0400

    merge error

--- parkvue/src/App.js
+++ parkvue/src/App.js
@@ -5,7 +5,6 @@ import HomeBanner from './Components/HomeBanner/HomeBanner';
 import MapSelect from './Components/HomeAd/MapSelect';
 import Reserve from './Components/HomeAd/Reserve';
 import UploadListing from './Components/HomeAd/UploadListing';
-<<<<<<< HEAD
 import {Route, Routes} from 'react-router-dom'
 import Home from './Pages/Home';
 import Login from './Pages/Login';
@@ -49,33 +48,7 @@ function App() {
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css"/>
             {/* Link To JS */}
         </div>
-=======
-import FAQ from './Components/FAQ';
-
-function App() {
-  return (
-    <div className="App">
-      <title>parkvue</title>
-        {/* Box Icons */}
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css" />
-        {/* NavBar */}
-        <NavBar/>
-        {/* Home Banner*/}
-       <HomeBanner/>
-       {/* Search Menu */}
-       <SearchMenu/>
-        {/* Home Ad: Reserve a Spot Service*/}
-        <Reserve/>
-        {/* Home Ad: Upload Listing Service */}
-        <UploadListing/>
-        {/*Home Ad:  Map Selection Services */}
-        <MapSelect/>
-        {/* Link To JS */}
-       <FAQ /> 
-      </div>
->>>>>>> e1fc9d1d657c15933dc8d7f4278cdd47cd419ea1
-    );
+    )
 }
-
 
 export default App;
