fbe92707227fb1aff644b2305eff266ce9c8d7af
medgardo <medgardo@gmail.com>
2023-11-17 20:00:14 -0500

    enable cors

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ mongoose.connect(process.env.MONGO_URL)
 try {
     //Middleware, recognizes as JSON object.
     //app.use(bodyparser.json());
-    //app.use(cors());
+    app.use(cors());
     app.use(express.json());
     app.use(cookieParser())
     app.use(express.urlencoded({extended: false}))
