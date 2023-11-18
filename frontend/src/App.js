83a9b7c5b01287a103f214c80856912d50d387f1
medgardo <edgardo@cunytechprep.org>
2023-11-17 19:00:02 -0500

    Update axios baseURL to use environment variable

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,7 @@ import FAQ from './Components/FAQ';
 import AboutPage from "./Pages/AboutPage";
 
 // connects frontend to backend
- axios.defaults.baseURL = "http://localhost:8000";
+ axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 axios.defaults.withCredentials = true;
 
 function App() {
