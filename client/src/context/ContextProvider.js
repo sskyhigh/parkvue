bd073069ede2a946b93d0a01645f55e133daeeaa
Kami840 <56164472+Kami840@users.noreply.github.com>
2023-12-02 01:23:48 -0500

    loading circle

--- client/src/context/ContextProvider.js
+++ client/src/context/ContextProvider.js
@@ -2,11 +2,12 @@
 import {createContext, useContext, useReducer } from 'react';
 import reducer from './reducer';
 
-//Engine state: Initial state contains all our public values
+//Engine global variable state: Initial state contains all our public values set false as default
 const initialState = {
     currentUser: null,
     openLogin: false,
-    alert: {open:false, severity:'info', message:' '},
+    loading: false,
+    alert: { open: false, severity: 'info', message: '' },
 };
 
 //when initial state is passed
