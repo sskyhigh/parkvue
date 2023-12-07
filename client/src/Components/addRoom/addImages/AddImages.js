2a28a9b317a10f8d67c9f496e01dbed92ca36998
sskyhigh <98570636+sskyhigh@users.noreply.github.com>
2023-12-06 22:00:42 -0500

    changes

--- client/src/Components/addRoom/addImages/AddImages.js
+++ client/src/Components/addRoom/addImages/AddImages.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, {useCallback, useState} from 'react';
+import {Paper} from "@mui/material";
+import {useDropzone} from "react-dropzone";
 
 const AddImages = () => {
+    const [files, setFiles] = useState([]);
+    const onDrop = useCallback((acceptedFiles) => {
+        setFiles(acceptedFiles);
+        console.log(acceptedFiles);
+    }, [])
+    const {getRootProps, getInputProps, isDragActive} = useDropzone({
+        onDrop,
+        accept: {'image/*':[]}
+    })
     return (
         <div>
-            add images
+            <Paper
+                sx={{
+                    cursor: 'pointer',
+                    background: '#fafafa',
+                    color: '#bdbdbd',
+                    border: '1px dashed #ccc',
+                    '&:hover': {border: '1px solid #ccc'}
+                }}
+            >
+                <div style={{padding: '16px'}}  {...getRootProps()}>
+                    <input {...getInputProps()}/>
+                    {isDragActive ? (
+                        <p style={{color: 'green'}}>Drop files here.</p>
+                    ) : (
+                        <p>Drag and drop some files here</p>
+                    )}
+                    <em>(Images with jpeg png are accepted)</em>
+                </div>
+            </Paper>
         </div>
     );
 };
